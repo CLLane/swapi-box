48c3ce9fa72ee4feecded34e22dc373c7adb39fb
Chris Lane <pankokrumbs@gmail.com>
2019-08-20 16:03:39 -0600

    Add a skeleton for person object

--- src/App/App.js
+++ src/App/App.js
@@ -6,16 +6,62 @@ class App extends Component {
     super()
     this.state = ({
       data: [],
-      people: []
+      people: [],
+      planets: [],
+      vehicles: [],
+      films: []
     })
   }
   componentDidMount() {
     fetch('https://swapi.co/api/')
     .then(response => response.json())
-    .then(data => console.log(data)) 
+    .then(data => this.fetchAll(data))
+    .then(data => this.fetchPersonInfo(this.state.people))
+    .then(data => this.setState({data: data}))
     .catch(err => console.log(err))
   }
+  
+  fetchAll = (data) => {
+    const people = 
+    fetch(data.people)
+      .then(response => response.json())
+      .then(characters => this.setState({people: characters}))
+    
+    const planets = fetch(data.planets)
+      .then(response => response.json())
+      .then(planet => this.setState({ planets: planet }));
 
+    const vehicles = fetch(data.vehicles)
+      .then(response => response.json())
+      .then(vehicle => this.setState({ vehicles: vehicle }));
+
+    const films = fetch(data.films)
+      .then(response => response.json())
+      .then(film => this.setState({ films: film }));
+    
+      return Promise.all([people, planets, vehicles, films])
+    }
+    
+    fetchPersonInfo = (people) => {
+      const homeWorlds = people.results.map(person => {
+        return fetch(person.homeworld)
+        .then(response => response.json())
+        .then(data => ({homeworld: data.name, name: person.name}))
+      })
+
+        const species = people.results.map((person, i) => {
+          return fetch(person.species)
+            .then(response => response.json())
+            .then(type => ({
+              name: person.name,
+              homeworld: homeWorlds[i],
+              species: type.name
+            }));
+        });
+      setTimeout(() => {console.log(species)}, 6000)
+      // return Promise.all(homeWorlds)
+  }
+    
   //Data needed for film
   //opening_crawl(data.films.results.opening_crawl)
 
