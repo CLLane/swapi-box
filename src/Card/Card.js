7666bf862564d720884fa61ef4a7c89309076dbc
Emily <lalonde0902@gmail.com>
2019-08-21 16:34:56 -0600

    Add info for people, planets, and vehicle to the DOM

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ name, homeworld, species, population }) => {
+const Card = ({ name, homeworld, species, population, terrain, climate, residents, model, vehicleClass, numberOfPassengers }) => {
 
   return (
     <div>
+    <section className="card">
       <h1>{name}</h1>
-      <p>{homeworld}</p>
+      <p>{homeworld || null}</p>
+      <p>{species || null}</p>
+      <p>{population || null}</p>
+      <p>{terrain || null}</p>
+      <p>{climate || null}</p>
+      <p>{residents || null}</p>
+      <p>{model || null}</p>
+      <p>{vehicleClass || null}</p>
+      <p>{numberOfPassengers || null}</p>
+    </section>
     </div>
   )
 };
