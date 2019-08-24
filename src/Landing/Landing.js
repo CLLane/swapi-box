b255a6979914f1b137ca9c6c4e51c24a3086cb1c
Chris Lane <pankokrumbs@gmail.com>
2019-08-23 18:34:20 -0600

    Add crawler css

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './Landing.css'
 import { NavLink } from 'react-router-dom';
 
 const Landing = ({film}) => {
   console.log(film);
   return (
-    <section>
-      {film}
-      <NavLink to="/" className="nav">
-        May the Force be with you
-      </NavLink>
+    <section className='crawler-section'>
+      <div className='crawler-div'>
+        <p>{film}</p>
+        {/* <NavLink to="/" className="nav">
+          May the Force be with you
+        </NavLink> */}
+      </div>
     </section>
   );
 }
