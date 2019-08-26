7396bd8878a6d7520ba7c76c98646c7355b973e6
Emily <lalonde0902@gmail.com>
2019-08-26 14:36:03 -0600

    Add display that shows how many cards have been favorited

--- src/App/App.js
+++ src/App/App.js
@@ -199,6 +199,7 @@ class App extends Component {
               className="character-icon"
             />
             Favorites
+            <span>{this.state.favorites.length}</span>
           </NavLink>
         </header>
         <Route exact path="/" component={Home} />
